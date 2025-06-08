e4718f6064e9188c260d5c1d7e2f834f357e9921
Daniela Castro <dcastrog1@eafit.edu.co>
2025-06-08 12:12:01 -0500

    Actualizar dependencias del proyecto y configurar docker compose para el backend

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3fa75976a42000a4a5b747267814f66831f860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a895f3280d161d03a80e727b056920f833b03a00")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
